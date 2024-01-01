5e02eb199034472c75bcd23deae5ab44778ef78f
MohnedMoneam0 <147365459+MohnedMoneam0@users.noreply.github.com>
2024-01-01 07:14:36 +0200

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -11,26 +11,30 @@ import threading
 
 from matrice_actiontracker import RPC
 from predict import load_model,predict 
+from python_common.services.rpc import Rpc
 
 class MatriceModel:
 
     def __init__(self, action_id, port):
         
         self.action_id = action_id
+        self.internal_rpc=Rpc()
+        self.action_doc=self.internal_rpc.get(f"/internal/project/v1/action/{self.action_id}/details")
+        self.action_details=self.action_doc['actionDetails']
+        print(self.action_details)
         self.rpc=RPC("mohned.moneam@matrice.ai",'mamoez12345#')
-        self.action_details=self.rpc.get("/internal/project/v1/action/{self.action_id}/details")
-        
-        self._idDeploymentInstance=self.action_details['_idService']
-        self._idDeployment=self.action_details['actionDetails']['_idDeployment']
-        self.model_id=self.action_details['actionDetails']['_idModelDeploy']
+        self._idDeploymentInstance=self.action_doc['_idService']
+        self._idDeployment=self.action_details['_idDeployment']
+        self.model_id=self.action_details['_idModelDeploy']
         
         
         self.model = None
         self.last_no_inference_time = -1
-        self.shutdown_on_idle_threshold = 3000
+        self.shutdown_on_idle_threshold = int(self.action_details['shutdownThreshold']) *60 
         self.app = FastAPI()
         self.ip = self.get_ip()
-       # self.run_shutdown_checker()
+        self.port=port
+        self.run_shutdown_checker()
         
         @self.app.post("/inference/")
         async def serve_inference(image: UploadFile = File(...)):
@@ -43,8 +47,15 @@ class MatriceModel:
                 return JSONResponse(content=jsonable_encoder({"status": 0, "message": "Some error occurred"}), status_code=500)
 
 
-    def run_api(self, host="0.0.0.0", port=80):
-        uvicorn.run(self.app, host=host, port=port)
+    def run_api(self,):
+        host="0.0.0.0"
+        port=80
+        self.update_deployment_address()
+        try:
+            self.update_status("deploy_add","deployment","MDL_DPL_STR", "OK", "Model deployment started")
+            uvicorn.run(self.app, host=host, port=port)
+        except:
+            self.update_status("deploy_add","deployment","ERROR", "ERROR", "Model deployment ERROR")
 
 
     def get_ip(self):
@@ -56,7 +67,7 @@ class MatriceModel:
     def inference(self, image):
         
         if self.model is None:
-            self.model = load_model()
+            self.model = load_model(self.model_id)
 
         self.last_no_inference_time = -1
 
@@ -67,7 +78,21 @@ class MatriceModel:
         except Exception as e:
             print(f"ERROR: {e}")
             return None, False
+            
+    def update_status(self, action,service_name,stepCode, status, status_description):
 
+        url= "/internal/project/v1/action"
+    
+        payload = {
+            "_id":self.action_id,
+            "action"  : action,
+            "serviceName": service_name,
+            "stepCode": stepCode,
+            "status":status,
+            "statusDescription":status_description,
+        }
+    
+        self.internal_rpc.put(url, payload)
 
     def trigger_shutdown_if_needed(self):
         if self.last_no_inference_time == -1:
@@ -77,9 +102,13 @@ class MatriceModel:
             if elapsed_time > int(self.shutdown_on_idle_threshold):
                 try:
                     print('Shutting down due to idle time exceeding the threshold.')
-                    os.system('shutdown now')
-                except:
-                    print("Unable to process shutdown")
+                    self.rpc.delete(f"/v1/deployment/delete_deploy_instance/{self._idDeploymentInstance}")
+                    self.update_status("deploy_add","deployment","MDL_DPL_STP", "OK", "Model deployment STOP")
+                    time.sleep(10)
+                    #os.system('exit')
+                    sys.exit()
+                except Exception as e:
+                    print(f"Error during shutdown: {e}")
                 sys.exit(1)
             else:
                 print('Time since last inference:', elapsed_time)
@@ -89,7 +118,7 @@ class MatriceModel:
     def shutdown_checker(self):
         while True:
             self.trigger_shutdown_if_needed()
-            time.sleep(60)
+            time.sleep(10)
 
 
     def run_shutdown_checker(self):
@@ -98,7 +127,7 @@ class MatriceModel:
         t1.start()
 
 
-    def update_deployment_info(self):
+    def update_deployment_address(self):
         ip = self.get_ip()
         port = self.port
         
@@ -113,5 +142,12 @@ class MatriceModel:
         
         self.rpc.put(path=url,payload=payload)
 
-x=MatriceModel("",8000)
-x.run_api()
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 deploy.py <action_status_id> <port>")
+        sys.exit(1)
+    
+    action_status_id = sys.argv[1]
+    port=sys.argv[2]
+    x=MatriceModel(action_status_id,port)
+    x.run_api()
